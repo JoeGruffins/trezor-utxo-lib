trezor/bitgo-utxo-lib

multi: Add decred.
Add decred coin parameters for testnet, simnet, and mainnet.

Add support for decred's unique hashing algorithm. The npm module
hash-lib is used for blake256 as it is already used in the decred
repositories.

Add a new module bs58checkBlake256 that adds support for decred encoding
and decoding. Appropriate tests are added to test all success and error
paths.

Allow decred's two byte wif prefix in networks.

src/coins.js
@@ -13,7 +13,8 @@ const coins = {
   CAPRICOIN: 'cpc',
   PEERCOIN: 'ppc',
   PEERCOINTEST: 'tppc',
-  KMD: 'kmd'
+  KMD: 'kmd',
+  DCR: 'dcr'
 }
 
 coins.isBitcoin = function (network) {
@@ -64,6 +65,10 @@ coins.isPeercoin = function (network) {
   return typeforce.value(coins.PEERCOIN)(network.coin) || typeforce.value(coins.PEERCOINTEST)(network.coin)
 }
 
+coins.isDecred = function (network) {
+  return typeforce.value(coins.DCR)(network.coin)
+}
+
 coins.hasTimestamp = function (network) {
   return this.isCapricoin(network) || this.isPeercoin(network)
 }
@@ -78,7 +83,8 @@ coins.isValidCoin = typeforce.oneOf(
   coins.isKomodo,
   coins.isZcashType,
   coins.isCapricoin,
-  coins.isPeercoin
+  coins.isPeercoin,
+  coins.isDecred
 )
 
 module.exports = coins

JoeGruffins/trezor-utxo-lib

src/coins.js
@@ -8,7 +8,8 @@ const coins = {
   BTG: 'btg',
   LTC: 'ltc',
   ZEC: 'zec',
-  DASH: 'dash'
+  DASH: 'dash',
+  DCR: 'dcr'
 }
 
 coins.isBitcoin = function (network) {
@@ -41,7 +42,8 @@ coins.isValidCoin = typeforce.oneOf(
   coins.isBitcoinSV,
   coins.isBitcoinGold,
   coins.isLitecoin,
-  coins.isZcash
+  coins.isZcash,
+  coins.isDecred
 )
 
 module.exports = coins